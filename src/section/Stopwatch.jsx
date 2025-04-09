e6e063eae3335699be35c0aa71f04d3e7a384a07
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2025-04-10 03:27:59 +0530

    added reset button

--- src/section/Stopwatch.jsx
+++ src/section/Stopwatch.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { PiPauseBold, PiPlayBold } from 'react-icons/pi';
 import { motion } from 'motion/react';
+import { BiReset } from 'react-icons/bi';
 
+const initialTime = { sec: 0, min: 0, hour: 0 };
 const Stopwatch = () => {
   const [isStarted, setIsStarted] = useState(false);
-  const [time, setTime] = useState({ sec: 0, min: 0, hour: 0 });
+  const [time, setTime] = useState(initialTime);
+  const [isReset, setIsReset] = useState(false);
 
   const handleClick = () => {
     setIsStarted(!isStarted);
@@ -46,14 +49,32 @@ const Stopwatch = () => {
           <div className='md:text-9xl dark:text-gray-300 text-7xl font-bold'>{formatTime(time.hour)}:{formatTime(time.min)}:{formatTime(time.sec)}</div>
           <div className='text-lg md:text-2xl text-right dark:text-gray-300 text-gray-500'>HH:MM:SS</div>
         </div>
-        <div className=' flex justify-center'>
+        <div className=' flex justify-center items-center gap-4 relative'>
+          {!isStarted &&
+            <motion.div
+              initial={{ opacity: 0, x: 0 }}
+              whileHover={{ boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.1)' }}
+              animate={{ opacity: 1, x: -110 }}
+              transition={{ duration: 0.2, ease: 'easeInOut' }}
+              className='bg-red-700 select-none text-white cursor-pointer absolute z-10 flex items-center justify-center gap-4 w-fit p-3 px-7 rounded-full shadow-lg'
+              onClick={() => {
+                setIsReset(!isReset);
+                setTime(initialTime);
+              }
+              }
+            >
+              <BiReset size={20} />
+              {/* <p className='text-xl opacity-70'>Reset</p> */}
+            </motion.div>
+          }
+
           <motion.div
             initial={{ opacity: 0, y: -50 }}
             whileHover={{ boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.1)' }}
             animate={{ opacity: 1, y: 0 }}
             onClick={handleClick}
             transition={{ duration: 0.5, ease: 'easeInOut' }}
-            className={` flex items-center justify-center cursor-pointer gap-4 ${isStarted ? "bg-gray-400" : "bg-red-300"} w-fit p-2 px-5 rounded-full shadow-lg`}
+            className={` flex items-center select-none justify-center cursor-pointer gap-4 ${isStarted ? "bg-gray-600 text-green-200" : "bg-green-600 text-white"} w-fit p-2 px-5 rounded-full shadow-lg`}
           >
             {isStarted
               ? <>
@@ -67,6 +88,7 @@ const Stopwatch = () => {
             }
 
           </motion.div>
+
         </div>
       </div>
     </div>
